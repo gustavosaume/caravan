5e49fd3ce4d56d03f5ccea9726da94744bd154a9
Gustavo Saume <gustavosaume@gmail.com>
2015-04-01 23:08:35 -0400

    Makes shelter list the new landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
   resources :shelters do
     resources :testimonials, only: [:create]
   end
 
-  root 'welcome#index'
+  root 'shelters#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
