0175f6fb3a90027eb264e9e2145991f7bcc17ed2
Gustavo Saume <gustavosaume@gmail.com>
2015-04-13 10:44:50 -0400

    Updates banner icons

--- app/assets/javascripts/global-events.js
+++ app/assets/javascripts/global-events.js
@@ -6,4 +6,11 @@ $(document).ready(function() {
   $("body").on("click", ".coming-soon", function() {
     window.alert("Coming soon!");
   });
+
+  $("body").on("click", ".scrollable", function() {
+    var self = this;
+    $("html, body").animate({
+      scrollTop: $($(self).data("scroll-to-selector")).offset().top
+    }, 500);
+  });
 });
