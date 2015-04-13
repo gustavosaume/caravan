0ccb6823fa85b071fa32294fdca738ea46d22815
Gustavo Saume <gustavosaume@gmail.com>
2015-04-13 11:10:25 -0400

    Random avatars for everybody

--- app/controllers/testimonials_controller.rb
+++ app/controllers/testimonials_controller.rb
@@ -16,6 +16,35 @@ class TestimonialsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def testimonial_params
-      params.require(:testimonial).permit(:author_name, :shelter_id, :complaint, :compliment, :tips, :services, :shelter, :rating).merge(shelter: @shelter)
+      params.require(:testimonial).permit(
+        :author_name, 
+        :shelter_id, 
+        :complaint, 
+        :compliment, 
+        :tips, 
+        :services, 
+        :shelter, 
+        :rating,
+        :thumbnail
+      )
+      .merge(shelter: @shelter)
+      .merge(thumbnail: random_thumbnail)
+    end
+
+    def random_thumbnail
+      [
+        "/images/avatars/Avatar0.png",
+        "/images/avatars/Avatar1.png",
+        "/images/avatars/Avatar2.png",
+        "/images/avatars/Avatar3.png",
+        "/images/avatars/Avatar4.png",
+        "/images/avatars/Avatar5.png",
+        "/images/avatars/Avatar6.png",
+        "/images/avatars/Avatar7.png",
+        "/images/avatars/Avatar8.png",
+        "/images/avatars/Avatar9.png",
+        "/images/avatars/Avatar10.png",
+        "/images/avatars/Avatar11.png",
+      ].sample
     end
 end
